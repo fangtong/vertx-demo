3639fcd84ef4181713eaab30e9c1e6adf06a8916
fangtong <fangtong8752@gmail.com>
2017-05-31 15:56:25 +0800

    fix bug 并把测试代码提出

--- src/main/java/org/ft/test/vertx/jdbc/JdbcVerticle.java
+++ src/main/java/org/ft/test/vertx/jdbc/JdbcVerticle.java
@@ -11,8 +11,10 @@ import org.ft.test.vertx.utils.AsyncResultUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 
 /**
@@ -21,20 +23,9 @@ import java.util.function.Function;
 public class JdbcVerticle extends AbstractVerticle {
 
     public static void main(String[] args) {
-        Vertx.vertx().deployVerticle(new JdbcVerticle());
-    }
-
-    volatile int cnt = 0;
-    volatile int error = 0;
-
-    BlockingQueue<Integer> mobileNumbers = new LinkedBlockingQueue<>();
-
-    JDBCClient shared;
-
-    @Override
-    public void start(Future<Void> startFuture) throws Exception {
-
-        shared = JDBCClient.createShared(vertx, new JsonObject()
+        JdbcVerticle jdbc = new JdbcVerticle();
+        Vertx vertx = Vertx.vertx();
+        DeploymentOptions deploymentOptions = new DeploymentOptions().setConfig(new JsonObject()
                 .put("url", "jdbc:mysql://172.31.16.9:33061/?useUnicode=true&characterEncoding=UTF-8")
                 .put("driver_class", "com.mysql.jdbc.Driver")
                 .put("user", "root")
@@ -42,54 +33,49 @@ public class JdbcVerticle extends AbstractVerticle {
                 .put("max_pool_size", 30)
                 .put("row_stream_fetch_size", 10000));
 
-//        Future.<SQLConnection>future(f -> {
-//            shared.getConnection(f.completer());
-//        }).compose(connection -> {
-//            Future<SQLConnectionHold<SQLRowStream>> f = Future.future();
-//            connection.queryStream("select mobileNumber from db_sdk_location.t_phone_province where mobileCity is null", conv(f, connection));
-//            return f;
-//        }).compose(result -> {
-//            SQLRowStream stream = result.body;
-//            stream.handler(row -> {
-//                Integer head = row.getInteger(0);
-//                mobileNumbers.add(head);
-//            });
-//
-//            return Future.succeededFuture(result.connect);
-//        })
-//                .setHandler(result -> {
-//                    if (result.succeeded()) {
-//                        result.result().close();
-//                    } else {
-//                        System.out.println(result.cause().getMessage());
-//                    }
-//                });
-        query("select mobileNumber from db_sdk_location.t_phone_province where mobileCity is null",
-                result -> {
-                    if (result.succeeded()) {
-                        List<JsonArray> rows = result.result().getResults();
-                        rows.forEach(row -> mobileNumbers.add(row.getInteger(0)));
-                    }
-                });
+        AtomicInteger error = new AtomicInteger();
+        AtomicInteger cnt = new AtomicInteger();
 
-        vertx.setPeriodic(1000, tt -> {
-            Integer head = mobileNumbers.poll();
-            if (head != null) {
-                vertx.executeBlocking(handler -> {
-                    vertx.createHttpClient().getNow(10002, "172.31.16.13", "/phone?phone=" + head + "0000", r -> {
-                        r.bodyHandler(buffer -> handler.complete(buffer.toString()));
+        BlockingQueue<Integer> mobileNumbers = new LinkedBlockingQueue<>();
+
+        vertx.deployVerticle(jdbc,deploymentOptions,deployResult->{
+            System.out.println("deploy suc");
+
+            jdbc.query("select mobileNumber from db_sdk_location.t_phone_province where mobileCity is null limit 1",
+                    result -> {
+                        if (result.succeeded()) {
+                            List<JsonArray> rows = result.result().getResults();
+                            rows.forEach(row -> mobileNumbers.add(row.getInteger(0)));
+                        }else{
+                            System.out.println("error:"+result.cause());
+                        }
                     });
-                }, false, rps -> {
-                    Map<String, String> result = Json.decodeValue((String) rps.result(), Map.class);
-                    if (result.get("city") == null
-                            || result.get("city").equals("null")) {
-                        ++error;
-                    }
-                    System.out.println(head + "/" + rps.result() + (++cnt) + "," + error);
-                });
-            }
-        });
 
+            vertx.setPeriodic(1000, tt -> {
+                Integer head = mobileNumbers.poll();
+                if (head != null) {
+                    vertx.executeBlocking(handler -> {
+                        vertx.createHttpClient().getNow(10002, "172.31.16.13", "/phone?phone=" + head + "0000", r -> {
+                            r.bodyHandler(buffer -> handler.complete(buffer.toString()));
+                        });
+                    }, false, rps -> {
+                        Map<String, String> result = Json.decodeValue((String) rps.result(), Map.class);
+                        if (result.get("city") == null
+                                || result.get("city").equals("null")) {
+                            error.incrementAndGet();
+                        }
+                        System.out.println(head + "/" + rps.result() + cnt.incrementAndGet() + "," + error.get());
+                    });
+                }
+            });
+        });
+    }
+
+    private JDBCClient shared;
+
+    @Override
+    public void start(Future<Void> startFuture) throws Exception {
+        shared = JDBCClient.createShared(vertx,config());
         super.start(startFuture);
     }
 
@@ -145,46 +131,24 @@ public class JdbcVerticle extends AbstractVerticle {
      * @param asyncResultHandler
      */
     public void query(String sql, Handler<AsyncResult<ResultSet>> asyncResultHandler) {
+        String uuid = UUID.randomUUID().toString();
         shared.getConnection(connresult -> {
             if (connresult.succeeded()) {
                 //只要连接成功 必须关闭connection
                 SQLConnection connection = connresult.result();
-                Future queryFuture = Future.<ResultSet>future(queryAsyncResult -> {
+                Future.<ResultSet>future(queryAsyncResult -> {
+                    connection.query(sql, queryAsyncResult.completer());
+                }).setHandler(resultSet->{
                     connection.close();
-                    asyncResultHandler.handle(queryAsyncResult);
+                    asyncResultHandler.handle(resultSet);
                 });
-                connection.query(sql, queryFuture.completer());
+
+                System.out.println(uuid+" connect suc!");
             } else {
                 asyncResultHandler.handle(AsyncResultUtil.transform(connresult, old -> null));
+                System.out.println(uuid+" connect fail!");
             }
         });
     }
 
-    <T> AsyncResult<T> conv(AsyncResult<SQLConnectionHold<T>> result) {
-        return AsyncResultUtil.transform(result, hold -> hold.body);
-    }
-
-
-    <T> Handler<AsyncResult<T>> conv(Future<SQLConnectionHold<T>> future, SQLConnection connection) {
-        return result -> {
-            if (result.succeeded()) {
-                future.complete(new SQLConnectionHold<>(connection, result.result()));
-            } else {
-                future.fail(result.cause());
-            }
-        };
-    }
-
-
-    class SQLConnectionHold<T> {
-        SQLConnection connect;
-        T body;
-
-        public SQLConnectionHold(SQLConnection connect, T result) {
-            this.connect = connect;
-            this.body = result;
-        }
-
-
-    }
 }
