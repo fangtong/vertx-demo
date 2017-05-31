0182f05a8caf913c7e723fe80399da381a7c3347
fangtong <fangtong8752@gmail.com>
2017-05-31 17:39:26 +0800

    不关心执行模板类型

--- src/main/java/org/ft/test/vertx/jdbc/JdbcVerticle.java
+++ src/main/java/org/ft/test/vertx/jdbc/JdbcVerticle.java
@@ -38,7 +38,7 @@ public class JdbcVerticle extends AbstractVerticle {
 
         BlockingQueue<Integer> mobileNumbers = new LinkedBlockingQueue<>();
 
-        vertx.deployVerticle(jdbc,deploymentOptions,deployResult->{
+        vertx.deployVerticle(jdbc, deploymentOptions, deployResult -> {
             System.out.println("deploy suc");
 
             jdbc.query("select mobileNumber from db_sdk_location.t_phone_province where mobileCity is null limit 1",
@@ -46,26 +46,27 @@ public class JdbcVerticle extends AbstractVerticle {
                         if (result.succeeded()) {
                             List<JsonArray> rows = result.result().getResults();
                             rows.forEach(row -> mobileNumbers.add(row.getInteger(0)));
-                        }else{
-                            System.out.println("error:"+result.cause());
+                        } else {
+                            System.out.println("error:" + result.cause());
                         }
                     });
 
             vertx.setPeriodic(1000, tt -> {
                 Integer head = mobileNumbers.poll();
                 if (head != null) {
-                    vertx.executeBlocking(handler -> {
-                        vertx.createHttpClient().getNow(10002, "172.31.16.13", "/phone?phone=" + head + "0000", r -> {
-                            r.bodyHandler(buffer -> handler.complete(buffer.toString()));
-                        });
-                    }, false, rps -> {
-                        Map<String, String> result = Json.decodeValue((String) rps.result(), Map.class);
-                        if (result.get("city") == null
-                                || result.get("city").equals("null")) {
-                            error.incrementAndGet();
-                        }
-                        System.out.println(head + "/" + rps.result() + cnt.incrementAndGet() + "," + error.get());
-                    });
+                    vertx.executeBlocking(
+                            handler -> vertx.createHttpClient().getNow(
+                                    10002, "172.31.16.13", "/phone?phone=" + head + "0000",
+                                    r ->  r.bodyHandler(buffer -> handler.complete(buffer.toString()))),
+                            false,
+                            rps -> {
+                                Map result = Json.decodeValue((String) rps.result(), Map.class);
+                                if (result.get("city") == null
+                                        || result.get("city").equals("null")) {
+                                    error.incrementAndGet();
+                                }
+                                System.out.println(head + "/" + rps.result() + cnt.incrementAndGet() + "," + error.get());
+                            });
                 }
             });
         });
@@ -75,25 +76,26 @@ public class JdbcVerticle extends AbstractVerticle {
 
     @Override
     public void start(Future<Void> startFuture) throws Exception {
-        shared = JDBCClient.createShared(vertx,config());
+        shared = JDBCClient.createShared(vertx, config());
         super.start(startFuture);
     }
 
 
     /**
      * 事务操作
-     * @param exec 事务中执行
-     * @param asyncResultHandler
+     *
+     * @param exec               事务中执行
+     * @param asyncResultHandler 事务处理结果
      */
-    public void transactions(Function<SQLConnection, Future<Void>> exec, Handler<AsyncResult<Boolean>> asyncResultHandler) {
+    public void transactions(Function<SQLConnection, Future<?>> exec, Handler<AsyncResult<Boolean>> asyncResultHandler) {
         //要保证连接内 不管是否异常 都要关闭连接
         shared.getConnection(connResult -> {
             if (connResult.succeeded()) {
                 SQLConnection connection = connResult.result();
                 //开启事务
-                Future.<Void>future(handle -> {
-                    connection.setAutoCommit(false, handle.completer());
-                }).compose(hold -> { //执行事务操作
+                Future.<Void>future(
+                        handle -> connection.setAutoCommit(false, handle.completer())
+                ).compose(hold -> { //执行事务操作
                     return exec.apply(connection);
                 }).compose(hold -> { //提交及回滚
                     Future<Boolean> f = Future.future();
@@ -127,8 +129,8 @@ public class JdbcVerticle extends AbstractVerticle {
     /**
      * 查询
      *
-     * @param sql
-     * @param asyncResultHandler
+     * @param sql                执行语句
+     * @param asyncResultHandler 异步结果
      */
     public void query(String sql, Handler<AsyncResult<ResultSet>> asyncResultHandler) {
         String uuid = UUID.randomUUID().toString();
@@ -136,17 +138,17 @@ public class JdbcVerticle extends AbstractVerticle {
             if (connresult.succeeded()) {
                 //只要连接成功 必须关闭connection
                 SQLConnection connection = connresult.result();
-                Future.<ResultSet>future(queryAsyncResult -> {
-                    connection.query(sql, queryAsyncResult.completer());
-                }).setHandler(resultSet->{
+                Future.<ResultSet>future(
+                        queryAsyncResult -> connection.query(sql, queryAsyncResult.completer())
+                ).setHandler(resultSet -> {
                     connection.close();
                     asyncResultHandler.handle(resultSet);
                 });
 
-                System.out.println(uuid+" connect suc!");
+                System.out.println(uuid + " connect suc!");
             } else {
                 asyncResultHandler.handle(AsyncResultUtil.transform(connresult, old -> null));
-                System.out.println(uuid+" connect fail!");
+                System.out.println(uuid + " connect fail!");
             }
         });
     }
