79773c9a117e8e881d7d83768ddc311b9bb1a11e
fangtong <fangtong8752@gmail.com>
2017-06-01 17:12:53 +0800

    jdbc 测试

--- src/main/java/org/ft/test/vertx/jdbc/JdbcVerticle.java
+++ src/main/java/org/ft/test/vertx/jdbc/JdbcVerticle.java
@@ -22,56 +22,6 @@ import java.util.function.Function;
  */
 public class JdbcVerticle extends AbstractVerticle {
 
-    public static void main(String[] args) {
-        JdbcVerticle jdbc = new JdbcVerticle();
-        Vertx vertx = Vertx.vertx();
-        DeploymentOptions deploymentOptions = new DeploymentOptions().setConfig(new JsonObject()
-                .put("url", "jdbc:mysql://172.31.16.9:33061/?useUnicode=true&characterEncoding=UTF-8")
-                .put("driver_class", "com.mysql.jdbc.Driver")
-                .put("user", "root")
-                .put("password", "Xingluo2015~@$")
-                .put("max_pool_size", 30)
-                .put("row_stream_fetch_size", 10000));
-
-        AtomicInteger error = new AtomicInteger();
-        AtomicInteger cnt = new AtomicInteger();
-
-        BlockingQueue<Integer> mobileNumbers = new LinkedBlockingQueue<>();
-
-        vertx.deployVerticle(jdbc, deploymentOptions, deployResult -> {
-            System.out.println("deploy suc");
-
-            jdbc.query("select mobileNumber from db_sdk_location.t_phone_province where mobileCity is null limit 1",
-                    result -> {
-                        if (result.succeeded()) {
-                            List<JsonArray> rows = result.result().getResults();
-                            rows.forEach(row -> mobileNumbers.add(row.getInteger(0)));
-                        } else {
-                            System.out.println("error:" + result.cause());
-                        }
-                    });
-
-            vertx.setPeriodic(1000, tt -> {
-                Integer head = mobileNumbers.poll();
-                if (head != null) {
-                    vertx.executeBlocking(
-                            handler -> vertx.createHttpClient().getNow(
-                                    10002, "172.31.16.13", "/phone?phone=" + head + "0000",
-                                    r ->  r.bodyHandler(buffer -> handler.complete(buffer.toString()))),
-                            false,
-                            rps -> {
-                                Map result = Json.decodeValue((String) rps.result(), Map.class);
-                                if (result.get("city") == null
-                                        || result.get("city").equals("null")) {
-                                    error.incrementAndGet();
-                                }
-                                System.out.println(head + "/" + rps.result() + cnt.incrementAndGet() + "," + error.get());
-                            });
-                }
-            });
-        });
-    }
-
     private JDBCClient shared;
 
     @Override
--- src/test/java/org/ft/test/vertx/jdbc/JdbcVerticleTest.java
+++ src/test/java/org/ft/test/vertx/jdbc/JdbcVerticleTest.java
@@ -2,7 +2,6 @@ package org.ft.test.vertx.jdbc;
 
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Future;
-import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonArray;
@@ -12,7 +11,6 @@ import io.vertx.ext.sql.SQLConnection;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
-import org.ft.test.vertx.netty.NettyServerVerticle;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -62,7 +60,7 @@ public class JdbcVerticleTest {
     @Test
     public void query(TestContext context) {
         Async async = context.async();
-        jdbc.query("select mobileNumber from db_sdk_location.t_phone_province where mobileCity is null limit 1",
+        jdbc.query("select * from test.test",
                 result -> {
                     if (result.succeeded()) {
                         List<JsonArray> rows = result.result().getResults();
@@ -76,7 +74,8 @@ public class JdbcVerticleTest {
 
 
     @Test
-    public void tran(TestContext context){
+    public void transactions(TestContext context){
+        //事务执行部分
         Function<SQLConnection, Future<?>> func = connection -> Future.<ResultSet>future(f->{
                 connection.query("select * from test.test",f.completer());
         }).compose(result->{
@@ -90,13 +89,66 @@ public class JdbcVerticleTest {
         });
 
 
+        //套入到事务中执行
         Async async = context.async();
         jdbc.transactions(
                 func,
                 result->{
+                    //返回事务执行结果 true为commit false为rollback
                     System.out.println("tran:"+result.succeeded());
                     async.complete();
                 }
         );
+    }
+
+
+    public static void main(String[] args) {
+        JdbcVerticle jdbc = new JdbcVerticle();
+        Vertx vertx = Vertx.vertx();
+        DeploymentOptions deploymentOptions = new DeploymentOptions().setConfig(new JsonObject()
+                .put("url", "jdbc:mysql://172.31.16.9:33061/?useUnicode=true&characterEncoding=UTF-8")
+                .put("driver_class", "com.mysql.jdbc.Driver")
+                .put("user", "root")
+                .put("password", "Xingluo2015~@$")
+                .put("max_pool_size", 30)
+                .put("row_stream_fetch_size", 10000));
+
+        AtomicInteger error = new AtomicInteger();
+        AtomicInteger cnt = new AtomicInteger();
+
+        BlockingQueue<Integer> mobileNumbers = new LinkedBlockingQueue<>();
+
+        vertx.deployVerticle(jdbc, deploymentOptions, deployResult -> {
+            System.out.println("deploy suc");
+
+            jdbc.query("select mobileNumber from db_sdk_location.t_phone_province where mobileCity is null limit 1",
+                    result -> {
+                        if (result.succeeded()) {
+                            List<JsonArray> rows = result.result().getResults();
+                            rows.forEach(row -> mobileNumbers.add(row.getInteger(0)));
+                        } else {
+                            System.out.println("error:" + result.cause());
+                        }
+                    });
+
+            vertx.setPeriodic(1000, tt -> {
+                Integer head = mobileNumbers.poll();
+                if (head != null) {
+                    vertx.executeBlocking(
+                            handler -> vertx.createHttpClient().getNow(
+                                    10002, "172.31.16.13", "/phone?phone=" + head + "0000",
+                                    r ->  r.bodyHandler(buffer -> handler.complete(buffer.toString()))),
+                            false,
+                            rps -> {
+                                Map result = Json.decodeValue((String) rps.result(), Map.class);
+                                if (result.get("city") == null
+                                        || result.get("city").equals("null")) {
+                                    error.incrementAndGet();
+                                }
+                                System.out.println(head + "/" + rps.result() + cnt.incrementAndGet() + "," + error.get());
+                            });
+                }
+            });
+        });
     }
 }
